f1fc68141c81a51220d3602322c4b4fa7b4fa573
Nguyễn Hải Đăng <71062801+nguyenhaidangyq@users.noreply.github.com>
2020-10-29 10:51:31 +0700

    update js

---
+++ Js/header-side.js
@@ -0,0 +1,17 @@
+function headerbox() {
+    return  " <section class=\"header-box-top\">\n" +
+        "             <img src=\"image/blog/logo%20cafe%20blog.png\">\n" +
+        "             <div class=\"buy-now-button\">\n" +
+        "                 <a href=\"#\">buy now</a>\n" +
+        "             </div>\n" +
+        "             <div class=\"menu-box-top\">\n" +
+        "                 <a href=\"#\">Menu</a>\n" +
+        "                 <a href=\"#\"><i class=\"fas fa-bars\"></i></a>\n" +
+        "             </div>\n" +
+        "         </section>\n" +
+        "         <section class=\"header-box-under\">\n" +
+        "            <h1>Blog</h1>\n" +
+        "         </section>"
+}
+var header=document.getElementById("header-box");
+header.innerText=headerbox();
